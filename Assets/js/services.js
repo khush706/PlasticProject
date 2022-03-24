706d977643d46af316e71151f0006422262504b4
Khush706 <khushjain706@gmail.com>
2022-03-24 13:44:39 +0530

    commit

--- Assets/js/services.js
+++ Assets/js/services.js
@@ -5,11 +5,11 @@ faceBtn.addEventListener("click", function() {
     let faceDisplay = document.getElementsByClassName("services_body_services_details_face_div_header_content");
     let display = window.getComputedStyle(faceDisplay[0]).display;
     if (display == "none") {
-        faceBtn.className = "fa-solid fa-angle-down";
+        faceBtn.className = "fa-solid fa-angle-up";
         document.getElementsByClassName("services_body_services_details_face_div_header_head")[0].style.backgroundColor = "whitesmoke";
         document.getElementsByClassName("services_body_services_details_face_div_header_content")[0].style.display = "block";
     } else {
-        faceBtn.className = "fa-solid fa-angle-up";
+        faceBtn.className = "fa-solid fa-angle-down";
         document.getElementsByClassName("services_body_services_details_face_div_header_head")[0].style.backgroundColor = "white";
         document.getElementsByClassName("services_body_services_details_face_div_header_content")[0].style.display = "none";
     }
@@ -20,11 +20,11 @@ bodyBtn.addEventListener("click", function() {
     let display = window.getComputedStyle(bodyDisplay[0]).display;
 
     if (display == "none") {
-        bodyBtn.className = "fa-solid fa-angle-down";
+        bodyBtn.className = "fa-solid fa-angle-up";
         document.getElementsByClassName("services_body_services_details_body_div_header_head")[0].style.backgroundColor = "whitesmoke";
         document.getElementsByClassName("services_body_services_details_body_div_header_content")[0].style.display = "block";
     } else {
-        bodyBtn.className = "fa-solid fa-angle-up";
+        bodyBtn.className = "fa-solid fa-angle-down";
         document.getElementsByClassName("services_body_services_details_body_div_header_head")[0].style.backgroundColor = "white";
         document.getElementsByClassName("services_body_services_details_body_div_header_content")[0].style.display = "none";
     }
