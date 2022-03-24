e732e8674545828d715b9d6a16d19db72be2edc0
Khush706 <khushjain706@gmail.com>
2022-03-24 18:11:28 +0530

    commit

--- Assets/js/services.js
+++ Assets/js/services.js
@@ -1,31 +1,46 @@
 var faceBtn = document.getElementById("face_drop_button");
 var bodyBtn = document.getElementById("body_drop_button");
 
+var qSelector = document.querySelectorAll(".services_body_services_details_inner_content_div");
+var header = document.querySelectorAll(".services_body_services_details_accordian_div_header_head");
+var iDown = document.querySelectorAll("iTag");
+
 faceBtn.addEventListener("click", function() {
-    let faceDisplay = document.getElementsByClassName("services_body_services_details_face_div_header_content");
-    let display = window.getComputedStyle(faceDisplay[0]).display;
+    resetAll();
+    var display = window.getComputedStyle(document.getElementById("face-inner-div")).display;
     if (display == "none") {
         faceBtn.className = "fa-solid fa-angle-up";
-        document.getElementsByClassName("services_body_services_details_face_div_header_head")[0].style.backgroundColor = "whitesmoke";
-        document.getElementsByClassName("services_body_services_details_face_div_header_content")[0].style.display = "block";
+        document.getElementById("face-div-header").style.backgroundColor = "whitesmoke";
+        document.getElementById("face-inner-div").style.display = "block";
     } else {
         faceBtn.className = "fa-solid fa-angle-down";
-        document.getElementsByClassName("services_body_services_details_face_div_header_head")[0].style.backgroundColor = "white";
-        document.getElementsByClassName("services_body_services_details_face_div_header_content")[0].style.display = "none";
+        document.getElementById("face-div-header").style.backgroundColor = "white";
+        document.getElementById("face-inner-div").style.display = "none";
     }
 });
 
 bodyBtn.addEventListener("click", function() {
-    let bodyDisplay = document.getElementsByClassName("services_body_services_details_body_div_header_content");
-    let display = window.getComputedStyle(bodyDisplay[0]).display;
-
+    resetAll();
+    var display = window.getComputedStyle(document.getElementById("body-inner-div")).display;
     if (display == "none") {
         bodyBtn.className = "fa-solid fa-angle-up";
-        document.getElementsByClassName("services_body_services_details_body_div_header_head")[0].style.backgroundColor = "whitesmoke";
-        document.getElementsByClassName("services_body_services_details_body_div_header_content")[0].style.display = "block";
+        document.getElementById("body-div-header").style.backgroundColor = "whitesmoke";
+        document.getElementById("body-inner-div").style.display = "block";
     } else {
         bodyBtn.className = "fa-solid fa-angle-down";
-        document.getElementsByClassName("services_body_services_details_body_div_header_head")[0].style.backgroundColor = "white";
-        document.getElementsByClassName("services_body_services_details_body_div_header_content")[0].style.display = "none";
+        document.getElementById("body-div-header").style.backgroundColor = "white";
+        document.getElementById("body-inner-div").style.display = "none";
     }
 });
+
+function resetAll() {
+    for (var i = 0; i < qSelector.length; i++) {
+        qSelector[i].style.display = "none";
+        header[i].style.backgroundColor = "white";
+    }
+
+    for (var u = 0; u < iDown.length; u++) {
+        iDown[u].className = "fa-solid fa-angle-down";
+    }
+
+}
