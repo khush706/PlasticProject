25e70e963b6fba845c5fa2ad9b0bdb97f99f1cb5
Khush706 <khushjain706@gmail.com>
2022-03-25 11:42:33 +0530

    commit

--- Assets/js/services.js
+++ Assets/js/services.js
@@ -3,44 +3,54 @@ var bodyBtn = document.getElementById("body_drop_button");
 
 var qSelector = document.querySelectorAll(".services_body_services_details_inner_content_div");
 var header = document.querySelectorAll(".services_body_services_details_accordian_div_header_head");
-var iDown = document.querySelectorAll("iTag");
+
+var iDownArrow = document.querySelectorAll(".fa-angle-down");
+var iUpArrow = document.querySelectorAll(".fa-angle-up");
 
 faceBtn.addEventListener("click", function() {
-    resetAll();
     var display = window.getComputedStyle(document.getElementById("face-inner-div")).display;
     if (display == "none") {
-        faceBtn.className = "fa-solid fa-angle-up";
-        document.getElementById("face-div-header").style.backgroundColor = "whitesmoke";
-        document.getElementById("face-inner-div").style.display = "block";
+        resetAll();
+        showDiv(faceBtn, "face-div-header", "face-inner-div");
     } else {
-        faceBtn.className = "fa-solid fa-angle-down";
-        document.getElementById("face-div-header").style.backgroundColor = "white";
-        document.getElementById("face-inner-div").style.display = "none";
+        hideDiv(faceBtn, "face-div-header", "face-inner-div");
     }
 });
 
 bodyBtn.addEventListener("click", function() {
-    resetAll();
     var display = window.getComputedStyle(document.getElementById("body-inner-div")).display;
     if (display == "none") {
-        bodyBtn.className = "fa-solid fa-angle-up";
-        document.getElementById("body-div-header").style.backgroundColor = "whitesmoke";
-        document.getElementById("body-inner-div").style.display = "block";
+        resetAll();
+        showDiv(bodyBtn, "body-div-header", "body-inner-div");
     } else {
-        bodyBtn.className = "fa-solid fa-angle-down";
-        document.getElementById("body-div-header").style.backgroundColor = "white";
-        document.getElementById("body-inner-div").style.display = "none";
+        hideDiv(bodyBtn, "body-div-header", "body-inner-div");
     }
 });
 
+
+function showDiv(iconBtn, headerDiv, innerDiv) {
+    iconBtn.className = "fa-solid fa-angle-up";
+    document.getElementById(headerDiv).style.backgroundColor = "whitesmoke";
+    document.getElementById(innerDiv).style.display = "block";
+}
+
+function hideDiv(iconBtn, headerDiv, innerDiv) {
+    iconBtn.className = "fa-solid fa-angle-down";
+    document.getElementById(headerDiv).style.backgroundColor = "white";
+    document.getElementById(innerDiv).style.display = "none";
+}
+
 function resetAll() {
+    // Hiding Visible elements
     for (var i = 0; i < qSelector.length; i++) {
         qSelector[i].style.display = "none";
         header[i].style.backgroundColor = "white";
     }
-
-    for (var u = 0; u < iDown.length; u++) {
-        iDown[u].className = "fa-solid fa-angle-down";
+    //Changing up elements classes to down 
+    for (var u = 0; u < iDownArrow.length; u++) {
+        iDownArrow[u].className = "fa-solid fa-angle-down";
     }
-
+    for (var d = 0; d < iUpArrow.length; d++) {
+        iUpArrow[d].className = "fa-solid fa-angle-down";
+    }
 }
