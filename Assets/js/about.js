f75618a14c31fdd4d2e64b2508f2f8f5bd9cf634
Khush706 <khushjain706@gmail.com>
2022-03-29 15:47:08 +0530

    commit

--- Assets/js/about.js
+++ Assets/js/about.js
@@ -3,3 +3,12 @@ $("#iPlayBtn").on("click", function() {
     $(".grid_div_for_iframe_imageHolderDiv").css("display", "none");
     $("#iframe")[0].src += "?autoplay=1";
 });
+
+
+$("#playBtnVideo").on("click", function() {
+    $("#iframeDocVideo").css("display", "block");
+    $(".about__body__doctor-intro-thumbnail-div").css("display", "none");
+    $("#iframeDocVideo")[0].src += "?autoplay=1";
+});
+
+
